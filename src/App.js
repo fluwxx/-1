f20c448779cd98500c144bb811204869e39f49c2
fluwxx <aaronratinov@gmail.com>
2024-01-16 16:30:02 +0600

    dz 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const user = {
+  name: 'Obama, blueface, DaBaby',
+  imageURL: 'https://i.imgur.com/BBwaSV1.jpeg',
+  imageSize: 360,
+  info: "Первый афроамериканец, выдвинутый на пост президента США от одной из двух крупнейших партий[9], и первый в национальной истории глав государства темнокожий президент, а также президент с фамилией африканского и средним именем арабского этимологического происхождения. Обама — мулат, но, в отличие от большинства чёрных американцев, не потомок рабов, а сын студента из Кении и белой американки (Стэнли Энн Данхэм). Плюс этот братик подержал реперов, конечно не джус врлд,но и так сойдёт"
 }
 
-export default App;
+export default function Profile() {
+  return (
+      <>
+        <h1>{user.name}</h1>
+        <h2>{user.info}</h2>
+        <img className="avatar" src={user.imageURL} alt={"photo of" + user.name} style={{width: user.imageSize, height: user.imageSize}}
+        />
+      </>
+  )
+}
+
